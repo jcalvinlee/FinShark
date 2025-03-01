dac06567b79ca19a40ba241b68ffef9fcdfdfd24
jcalvinlee <j.calvinlee@live.com.sg>
2025-03-01 13:44:09 +0800

    controller setup

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,39 @@
+﻿using api.Data;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;    
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Stock> stocks = _context.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            Stock? stock = _context.Stocks.Find(id);
+            
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(stock);
+        }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+//builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,11 +20,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-
+app.MapControllers();
 
 app.Run();
